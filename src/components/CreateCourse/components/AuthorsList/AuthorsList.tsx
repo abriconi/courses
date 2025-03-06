756001e1840fc1140c1400827218dbb93ed814eb
Alyona Gepting <abriconi@gmail.com>
2025-03-06 16:25:37 +0200

    Created component AuthorsList

--- src/components/CreateCourse/components/AuthorsList/AuthorsList.tsx
+++ src/components/CreateCourse/components/AuthorsList/AuthorsList.tsx
@@ -10,11 +10,11 @@ interface AuthorsListProps {
 
 export const AuthorsList: React.FC<AuthorsListProps> = ({ authors, onAddAuthor }) => {
   return (
-    <div className={styles.authorsList}>
-      <h2>Available Authors</h2>
-      <ul>
+    <div className={styles.authors}>
+      <h2 className={styles.header}>Available Authors</h2>
+      <ul className={styles.authors__list}>
         {authors.map((author) => (
-          <li key={author.id}>
+          <li key={author.id} className={styles.author}>
             <span>{author.name}</span>
             <Button name={BUTTON_TEXT.addAuthor} onClick={() => onAddAuthor(author)} />
           </li>
